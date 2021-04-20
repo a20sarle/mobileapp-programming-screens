a7ecd5b26a365d93722f20190eeeec4a686ae90a
a20sarle <a20sarle.student@his.se>
2021-04-20 11:15:28 +0200

    Created new activity: activity_login.xml

--- app/src/main/java/com/example/screens/LoginActivity.java
+++ app/src/main/java/com/example/screens/LoginActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /* hämtar layout från activity_main */
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
