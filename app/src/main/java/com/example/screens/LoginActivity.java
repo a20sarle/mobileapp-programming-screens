ddcf285110cd3b131432ca3c673e8fda7e786921
a20sarle <a20sarle.student@his.se>
2021-04-20 11:36:50 +0200

    Created button on first screen

--- app/src/main/java/com/example/screens/LoginActivity.java
+++ app/src/main/java/com/example/screens/LoginActivity.java
@@ -3,6 +3,9 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG","Hej!");
+            }
+        });
     }
 }
